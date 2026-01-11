dc0d8307f5f281dc48ad47c60a77b22ed18450e2
golden-chen <33325840+golden-chen@users.noreply.github.com>
2026-01-12 00:44:54 +0800

    Update test.py

--- api/test.py
+++ api/test.py
@@ -1,1 +1,8 @@
+from http.server import BaseHTTPRequestHandler
 
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write("你好！這是從 test.py 微服務傳回的訊息。".encode('utf-8'))
